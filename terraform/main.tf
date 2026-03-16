776a9b9bec004cda402778acd051fdd79eac40e7
samir <samir@DedSec.lan>
2026-03-16 18:14:21 +0100

    looks ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,8 +32,9 @@ resource "libvirt_volume" "debian13_base" {
 resource "libvirt_domain" "debian_vm" {
   name   = var.vm_name
   type   = "kvm"
-  memory = var.vm_memory
-  vcpu   = var.vm_vcpu
+  memory      = var.vm_memory
+  memory_unit = "MiB"
+  vcpu        = var.vm_vcpu
 
   os = {
     type      = "hvm"
@@ -65,6 +66,9 @@ resource "libvirt_domain" "debian_vm" {
 
     interfaces = [
       {
+        model = {
+          type = "virtio"
+        }
         source = {
           network = {
             network = var.vm_network
