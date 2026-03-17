403ca48f60e2a4adc2dd438ad57d9e5f33158881
samir <samir@DedSec.lan>
2026-03-17 10:55:20 +0100

    change name module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "vm_k3s_master" {
-  source     = "./modules/vm"
+  source     = "./modules/vm_k3s"
   vm_name    = "k3s-Master"
   base_image = var.base_image
   memory     = 2048
@@ -7,7 +7,7 @@ module "vm_k3s_master" {
 }
 
 module "vm_k3s_worker_1" {
-  source     = "./modules/vm"
+  source     = "./modules/vm_k3s"
   vm_name    = "k3s-Worker-1"
   base_image = var.base_image
   memory     = 2048
@@ -15,7 +15,7 @@ module "vm_k3s_worker_1" {
 }
 
 module "vm_k3s_worker_2" {
-  source     = "./modules/vm"
+  source     = "./modules/vm_k3s"
   vm_name    = "k3s-Worker-2"
   base_image = var.base_image
   memory     = 2048
---
+++ terraform/modules/vm_k3s/outputs.tf
@@ -0,0 +1,3 @@
+output "vm_name" {
+  value = libvirt_domain.vm.name
+}
