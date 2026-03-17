520576c87d1eab3b5573bac1af9077f4bff5dbf8
samir <samir@DedSec.lan>
2026-03-17 15:12:17 +0100

    add generation fichier inventaire ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,24 @@ module "vm_k3s_worker_2" {
   ansible_user    = "lab"
   ssh_private_key = "../packer/default_id_ed25519"
 }
+
+resource "local_file" "ansible_inventory" {
+  filename = "../ansible/hosts.yml"
+  content  = <<-EOF
+    all:
+      vars:
+        ansible_user: lab
+        ansible_ssh_private_key_file: ../packer/default_id_ed25519
+      children:
+        masters:
+          hosts:
+            k3s-master:
+              ansible_host: ${module.vm_k3s_master.vm_ip}
+        workers:
+          hosts:
+            k3s-worker-1:
+              ansible_host: ${module.vm_k3s_worker_1.vm_ip}
+            k3s-worker-2:
+              ansible_host: ${module.vm_k3s_worker_2.vm_ip}
+  EOF
+}
